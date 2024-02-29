1f0c1410a0da8eaa5031a3d8fc20ef1866c04a6f
Yoisaki <340167767@qq.com>
2024-02-29 22:21:37 +0800

    feat(Falling Platform): add falling

--- 2D Platformer Game/Assets/Scripts/FallingPlatformMovement.cs
+++ 2D Platformer Game/Assets/Scripts/FallingPlatformMovement.cs
@@ -2,46 +2,87 @@ using UnityEngine;
 
 public class FallingPlatformMovement : MonoBehaviour
 {
-    [SerializeField] private bool shaking=true;
-    [SerializeField] private float shake_vol=0.3f;
-    [SerializeField] private float range=0.2f;
+    [SerializeField] private bool shaking = true;
+    [SerializeField] private float shakeVelocity = 0.3f;
+    [SerializeField] private float range = 0.2f;
+    [SerializeField] private float fallingTime = 0.5f;
     private Rigidbody2D rb;
-    private float shake_ubound,shake_lbound;
+    private BoxCollider2D coll;
+    private float shakeUbound, shakeLbound, destroyTime = 2f;
+    private bool falling = false, stomped = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb=GetComponent<Rigidbody2D>();
-        shake_ubound=transform.position.y+range;
-        shake_lbound=transform.position.y-range;
-        if(shaking)
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
+        shakeUbound = transform.position.y + range;
+        shakeLbound = transform.position.y - range;
+        if (shaking)
         {
-            rb.velocity=new Vector2(0,shake_vol);
+            rb.velocity = new Vector2(0, shakeVelocity);
         }
     }
 
     void Shake()
     {
-        if(transform.position.y>shake_ubound)
+        if (transform.position.y > shakeUbound)
         {
-            rb.velocity=new Vector2(0,-shake_vol);
+            rb.velocity = new Vector2(0, -shakeVelocity);
         }
-        if(transform.position.y<shake_lbound)
+        if (transform.position.y < shakeLbound)
         {
-            rb.velocity=new Vector2(0,shake_vol);
+            rb.velocity = new Vector2(0, shakeVelocity);
         }
+    }
+
+    bool IsStomped()
+    {
+        if (Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.up, .1f, 1 << LayerMask.NameToLayer("Player")))
+        {
+            stomped = true;
+        }
+        return stomped;
+    }
+
+    void SelfDestroy()
+    {
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(shaking)
+
+        if (IsStomped())
+        {
+            if (fallingTime < 0f)
+            {
+                falling = true;
+            }
+            else
+            {
+                fallingTime -= Time.deltaTime;
+            }
+        }
+
+        if (falling)
+        {
+            coll.enabled = false;
+            rb.bodyType = RigidbodyType2D.Dynamic;
+            Invoke(nameof(SelfDestroy),destroyTime);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(shaking && !falling)
         {
             Shake();
         }
-        else
+        else if(!shaking)
         {
-            rb.velocity=new Vector2(0,0);
+            rb.velocity = Vector2.zero;
         }
     }
 }
