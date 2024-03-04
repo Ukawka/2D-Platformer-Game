06e8f72d90e37f873b38e8ec1d28176ca06d4f61
Linzhq18 <zhq.lin.code@outlook.com>
2024-03-04 23:48:05 +0800

    refactor(Player Movement): make actual movements and animation updating more separate
    Actual movements (movements done in FixedUpdate()), which used to depend on AnimationState (MovementState), are now totally independent from it. Plus, animation updating is now more related to actual movement, rather than input and other state.

--- 2D Platformer Game/Assets/Scripts/PlayerMovement.cs
+++ 2D Platformer Game/Assets/Scripts/PlayerMovement.cs
@@ -2,28 +2,38 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-    enum MovementState { idle, run, jump, fall, wallSlide }
-
+    // reference
     SpriteRenderer sprite;
     Rigidbody2D rb;
     BoxCollider2D coll;
     Animator anim;
 
+    // parameter
     [SerializeField] float runSpeed = 600f;
     [SerializeField] float jumpSpeed = 1200f;
     [SerializeField] float wallSlideSpeed = 200f;
 
+    // input
     float hAxisInput = 0;
     bool jumpButtonPressed = false;
 
+    // collide state
     bool isGrounded = false;
-    bool isTouchingWall = false;
     bool isTouchingLeftWall = false;
     bool isTouchingRightWall = false;
-    MovementState movementState = MovementState.idle;
 
+    // to-do
     bool toJump = false;
+
+    // animation state
+    enum AnimationState { idle, run, rise, fall, wallSlide }
+    AnimationState animState = AnimationState.idle;
+
+    // actual move
+    bool isActMovingH = false; // is actively moving horizontally
+    bool isActMovingL = false; // is actively moving left
+    bool isActMovingR = false; // is actively moving right
+    bool isWallSliding = false;
 
     void Start()
     {
@@ -39,8 +49,7 @@ public class PlayerMovement : MonoBehaviour
         UpdateInput();
         UpdateCollideState();
         UpdateToDo();
-        UpdateMovementState();
-        UpdateAnimationState();
+        UpdateAnimation();
     }
 
     void UpdateInput()
@@ -54,83 +63,75 @@ public class PlayerMovement : MonoBehaviour
         isGrounded = Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, 1 << LayerMask.NameToLayer("Ground"));
         isTouchingLeftWall = Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.left, .1f, 1 << LayerMask.NameToLayer("Ground"));
         isTouchingRightWall = Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.right, .1f, 1 << LayerMask.NameToLayer("Ground"));
-        isTouchingWall = isTouchingLeftWall || isTouchingRightWall;
     }
 
     void UpdateToDo()
     {
         if (jumpButtonPressed && isGrounded)
-        {
             toJump = true;
-        }
     }
 
-    void UpdateMovementState()
+    void UpdateAnimation()
     {
-        // judge by input and collide state
         if (isGrounded)
         {
-            if (hAxisInput == 0)
-            {
-                movementState = MovementState.idle;
-            }
-            else
-            {
-                movementState = MovementState.run;
-            }
+            if (isActMovingH) // & grounded => running
+                animState = AnimationState.run;
+            else // grounded & not actively moving horizontally => idling
+                animState = AnimationState.idle;
         }
-        else if (rb.velocity.y < -.1f && ((isTouchingLeftWall && hAxisInput < 0) || (isTouchingRightWall && hAxisInput > 0)))
+        else if (isWallSliding) // => wall sliding
         {
-            movementState = MovementState.wallSlide;
+            animState = AnimationState.wallSlide;
         }
-        else
+        else // not grounded & not wall sliding => in the air
         {
-            if (rb.velocity.y > .1f)
-            {
-                movementState = MovementState.jump;
-            }
-            else if (rb.velocity.y < -.1f)
-            {
-                movementState = MovementState.fall;
-            }
+            if (rb.velocity.y > .1f) // in the air & moving upwards => rising
+                animState = AnimationState.rise;
+            else if (rb.velocity.y < -.1f) // in the air & moving downwards => falling
+                animState = AnimationState.fall;
         }
 
-        // judge by todos
-        if (toJump)
-        {
-            movementState = MovementState.jump;
-        }
-    }
+        anim.SetInteger("state", (int)animState);
 
-    void UpdateAnimationState()
-    {
-        anim.SetInteger("movement state", (int)movementState);
-
-        if (hAxisInput > 0)
-        {
-            sprite.flipX = false;
-        }
-        else if (hAxisInput < 0)
-        {
+        if (isActMovingL)
             sprite.flipX = true;
-        }
+        else if (isActMovingR)
+            sprite.flipX = false;
     }
 
     void FixedUpdate()
     {
-        // set velocity
+        // move horizontally
+        if (hAxisInput != 0)
+        {
+            rb.velocity = new Vector2(hAxisInput * runSpeed * Time.deltaTime, rb.velocity.y);
+            isActMovingH = true;
+            isActMovingL = hAxisInput < 0;
+            isActMovingR = hAxisInput > 0;
+        }
+        else
+        {
+            rb.velocity = new Vector2(hAxisInput * runSpeed * Time.deltaTime, rb.velocity.y);
+            isActMovingH = isActMovingL = isActMovingR = false;
+        }
 
-        rb.velocity = new Vector2(hAxisInput * runSpeed * Time.deltaTime, rb.velocity.y);
+        // wall slide
+        if (!isGrounded && ((isTouchingLeftWall && hAxisInput < 0) || (isTouchingRightWall && hAxisInput > 0)) && rb.velocity.y < -.1f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlideSpeed * Time.deltaTime, float.MaxValue));
+            isWallSliding = true;
+        }
+        else
+        {
+            isWallSliding = false;
+        }
 
+        // jump
         if (toJump)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed * Time.deltaTime);
             toJump = false;
-        }
-
-        if (movementState == MovementState.wallSlide)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlideSpeed * Time.deltaTime, float.MaxValue));
         }
     }
 }
